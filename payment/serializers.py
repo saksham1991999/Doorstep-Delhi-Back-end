6527a7ca1175953f8b5bae22fef0520f5509f2d3
Junaid Girkar <junaidgirkar@gmail.com>
2021-04-03 19:43:29 +0530

    Changed OrderSerializer location

--- payment/serializers.py
+++ payment/serializers.py
@@ -1,7 +1,36 @@
 from rest_framework import serializers
 from rest_framework.fields import CurrentUserDefault
 from payment.models import Transaction, Payment
-from shop.serializers import OrderSerializer
+from shop.models import Order
+
+
+from rest_framework import serializers
+from shop.models import Order
+from rest_framework import serializers
+
+
+class OrderSerializer(serializers.ModelSerializer):
+    user = serializers.HiddenField(default=serializers.CurrentUserDefault())
+
+    class Meta:
+        model = Order
+        fields = [
+            "created",
+            "status",
+            "user",
+            "tracking_client_id",
+            "billing_address",  # NEED SERIALIZER
+            "shipping_address",  # NEED SERIALIZER
+            "shipping_method",  # NEED SERIALIZER
+            "shipping_price",
+            "total_net_amount",
+            "undiscounted_total_net_amount",
+            "voucher",  # NEED TO ADD A SERIALIZER
+            "gift_cards",  # NEED A SERIALIZER
+            "display_gross_prices",
+            "customer_note",
+        ]
+
 
 
 class PaymentSerializer(serializers.ModelSerializer):
