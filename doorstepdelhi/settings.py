7ae69c57f0fb476e17fd5d94bd42f57cbfcb6e5a
Saksham Mittal <181210045@nitdelhi.ac.in>
2021-06-17 03:49:18 +0530

    Wishlist Consumer

--- doorstepdelhi/settings.py
+++ doorstepdelhi/settings.py
@@ -259,7 +259,7 @@ CELERY_RESULT_BACKEND = os.environ.get("CELERY_BROKER", "redis://redis:6379/0")
 CACHES = {
     "default": {
         "BACKEND": "django_redis.cache.RedisCache",
-        "LOCATION": os.environ.get("REDIS_URL", "redis://redis:6379/0"),
+        "LOCATION": config("REDIS_URL", "redis://redis:6379/0"),
         "OPTIONS": {
             "CLIENT_CLASS": "django_redis.client.DefaultClient",
         },
