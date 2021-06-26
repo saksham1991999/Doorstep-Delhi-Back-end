01cde1c99439624eeb5c663c2e65b37d2b0733c8
kevinujunior <65278039+kevinujunior@users.noreply.github.com>
2021-06-27 00:39:35 +0530

    add_to_cart added

--- product/views.py
+++ product/views.py
@@ -17,6 +17,7 @@ from django.views.decorators.vary import vary_on_cookie, vary_on_headers
 from django.core.cache.backends.base import DEFAULT_TIMEOUT
 from django.core.cache import cache
 from django.conf import settings
+from rest_framework.serializers import Serializer
 
 from product.models import (
     Category,
@@ -41,7 +42,7 @@ from product.serializers import *  # """ NEED TO CHANGE ASAP """
 from wishlist.models import Wishlist, WishlistItem
 from wishlist.serializers import WishlistSerializer
 from accounts.models import Address
-from shop.serializers import OrderLineSerializer
+from shop.serializers import OrderLineSerializer, OrderSerializer
 from shop.models import Order, OrderLine
 
 CACHE_TTL = getattr(settings, 'CACHE_TTL', DEFAULT_TIMEOUT)
@@ -216,46 +217,46 @@ class ProductVariantViewSet(viewsets.ModelViewSet):
         serializer = WishlistSerializer(wishlist, many=False)
         return Response(serializer.data, status=status.HTTP_200_OK)
     
-    @action(detail=True, methods=["get"], permission_classes=[IsAuthenticated])
+    @action(detail=True, methods=["get"], permission_classes=[IsAuthenticated])            #wholsale_min_qty constraint not applied
     def add_to_cart(self,request,pk):
         current_user = request.user
-
-        # user_address = get_object_or_404(Address, user=current_user)
-        print("DEFAULT ADDRESS: "+ str(request.user.default_billing_address))
-        order, is_created = Order.objects.get_or_create(user=current_user, billing_address=request.user.default_billing_address, shipping_address=request.user.default_shipping_address)
-        print("CURRENT ORDER: "+ str(order))
-        current_product_variant = get_object_or_404(ProductVariant, id = pk)
-
+        product_variant = self.get_object()
+        order = Order.objects.filter(user=current_user, billing_address=request.user.default_billing_address, shipping_address=request.user.default_shipping_address)
+        orderlines = OrderLine.objects.filter(variant = product_variant, order__in = order)
         
-        if(Order.objects.filter(user=current_user).exists()):
-            current_order, is_created = Order.objects.get_or_create(user=current_user)
-            if(OrderLine.objects.filter(order=current_order, variant=current_product_variant).exists()):
-                orderline, is_created = OrderLine.objects.get_or_create(order=current_order, variant=current_product_variant)
-                orderline_quantity = orderline.quantity
-                orderline.update(quantity= orderline_quantity+1)
-                orderline.save()
-
-                serializer = OrderLineSerializer(orderline)
-                return Response(serializer.data, status=status.HTTP_200_OK)
-
-            else:
-                orderline, is_created = OrderLine.objects.get_or_create(order=current_order, variant=current_product_variant, quantity=1)
-                orderline.save()
-            
-                serializer = OrderLineSerializer(orderline)
-                return Response(serializer.data, status=status.HTTP_200_OK)
-        else:
-            current_order, is_created = Order.objects.get_or_create(user=current_user)
-            current_product_variant = get_object_or_404(ProductVariant, id = pk)
-
-            orderline , is_created= OrderLine.objects.get_or_create(order=current_order, variant=current_product_variant, quantity=1)
-            orderline.save()
-            
-            serializer = OrderLineSerializer(orderline)
+        if(not (orderlines.exists() and order.exists())):
+            order= Order.objects.create(
+            user=current_user, 
+            billing_address=current_user.default_billing_address, 
+            shipping_address=current_user.default_shipping_address, 
+            status ="draft", 
+            )
+           
+            OrderLine.objects.create(variant = product_variant, 
+                                                       quantity = 1,
+                                                       quantity_fulfilled=0, 
+                                                       order = order)
+        
+    
+            serializer = OrderSerializer(order, many = False)
             return Response(serializer.data, status=status.HTTP_200_OK)
+        
+        else:
+            print("exist")
+            order = Order.objects.get(
+            user=current_user, 
+            billing_address=request.user.default_billing_address, 
+            shipping_address=request.user.default_shipping_address, 
+            status ="draft", 
+            )
+            orderline = OrderLine.objects.get(variant = product_variant,
+                                                       order = order)
+            
+            serializer = OrderSerializer(order, many = False)
+            return Response(serializer.data, status=status.HTTP_200_OK)
+            
 
    
-
 
 class WholesaleProductVariantViewSet(viewsets.ModelViewSet):
     serializer_class = WholesaleProductVariantSerializer
@@ -266,11 +267,11 @@ class WholesaleProductVariantViewSet(viewsets.ModelViewSet):
         return wholesale_product_variants
     
 
+
 class BrandViewSet(viewsets.ModelViewSet):
     serializer_class = BrandSerializer
     permission_classes = [IsAdminOrReadOnly]
     queryset = Brand.objects.all()
-
 
 
 
